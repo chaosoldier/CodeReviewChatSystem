6947ff7eb6495f81cd93a962d581b2e4795f56fa
vicugna <iprettyapple@icloud.com>
2015-01-02 18:27:52 +0900

    fix

--- sql/create_table_message_file.sql
+++ sql/create_table_message_file.sql
@@ -2,5 +2,5 @@ CREATE TABLE message_file(
 	message_file_id SERIAL PRIMARY KEY,
 	message_id INTEGER REFERENCES message(message_id) NOT NULL,
 	file_binary BYTEA NOT NULL,
-	file_name TEXT DEFAULT 'notsetting.png' NOT NULL,
+	file_name TEXT DEFAULT 'notsetting.png' NOT NULL
 )
