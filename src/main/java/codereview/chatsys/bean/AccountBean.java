c332e09a9a6b3fb6873d8cca3be354e078bc1e39
user340 <e.yuuki.car@gmail.com>
2015-01-20 23:28:04 +0900

    Update AccountBean.java
    目を通すついでにコメント追加

--- src/main/java/codereview/chatsys/bean/AccountBean.java
+++ src/main/java/codereview/chatsys/bean/AccountBean.java
@@ -9,15 +9,20 @@ import codereview.chatsys.definition.Encrypter;
  * accountテーブルに対応したBean
  * @author vicugna
  *
+ * コメントを追加 by user340
+ * 
  */
 @Value
 public class AccountBean implements Serializable{
-	private static final long serialVersionUID = 1215912304612055644L;
-	private int account_id;
-	private String email_address;
-	private String passphrase;
-	private Encrypter TYPE;
-	private boolean available;
+	private static final long serialVersionUID = 1215912304612055644L;	/* serialVersionUID */
+	private int account_id;		/* アカウントID */
+	private String email_address;	/* メールアドレス */
+	private String passphrase;	/* パスワード */
+	private Encrypter TYPE;		/* 暗号化関係の変数？ */
+	private boolean available;	/* システムの利用が可能なユーザかどうかを判別するための変数？ */
+	/*
+	 * ユーザの追加？定義？
+	 */
 	public AccountBean(int account_id, String email_address, String passphrase,
 			Encrypter tYPE, boolean available) {
 		super();
